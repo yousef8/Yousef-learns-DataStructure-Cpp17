90329d1159b9fd83af2ea11b66847af0522ceb24
Yousef Elsayed <yousef4511@outlook.com>
2023-07-28 19:19:38 +0300

    Refactor DLL 4th Easy HW to use smart pointers

--- 3_DLL/EasyHW/4_isPalindrome.cpp
+++ 3_DLL/EasyHW/4_isPalindrome.cpp
@@ -1,358 +1,84 @@
-#include <iostream>
-#include <cassert>
-#include <climits>
+#include "../basicDLL/dll.hpp"
+#include <initializer_list>
 
-#include <vector> // for debug
-#include <algorithm>
-#include <sstream>
-using namespace std;
-
-struct Node
-{
-    int data{};
-    Node *next{};
-    Node *prev{}; // Previous node!
-
-    Node(int data) : data(data) {}
-
-    void set(Node *next, Node *prev)
-    {
-        this->next = next;
-        this->prev = prev;
-    }
-
-    ~Node()
-    {
-        cout << "Destroy value: " << data << " at address " << this << "\n";
-    }
-};
-
-class LinkedList
-{
-private:
-    Node *head{};
-    Node *tail{};
-    int length = 0;
-
-    // let's maintain how many nodes
-
-    vector<Node *> debug_data; // add/remove nodes you use
-
-    void debug_add_node(Node *node)
-    {
-        debug_data.push_back(node);
-    }
-    void debug_remove_node(Node *node)
-    {
-        auto it = std::find(debug_data.begin(), debug_data.end(), node);
-        if (it == debug_data.end())
-            cout << "Node does not exist\n";
-        else
-            debug_data.erase(it);
-    }
-
+class DLL : public BasicDLL {
 public:
-    // Below 2 deletes prevent copy and assign to avoid this mistake
-    LinkedList()
-    {
-    }
-    LinkedList(const initializer_list<int> &lst)
-    {
-        for (auto &value : lst)
-            insert_end(value);
-    }
-    LinkedList(const LinkedList &) = delete;
-    LinkedList &operator=(const LinkedList &another) = delete;
+  DLL() = default;
 
-    void debug_print_address()
-    {
-        for (Node *cur = head; cur; cur = cur->next)
-            cout << cur << "," << cur->data << "\t";
-        cout << "\n";
-    }
+  DLL(const std::initializer_list<int> &lst) : BasicDLL(lst) {}
 
-    void debug_print_node(Node *node, bool is_seperate = false)
-    {
-        if (is_seperate)
-            cout << "Sep: ";
-        if (node == nullptr)
-        {
-            cout << "nullptr\n";
-            return;
-        }
+  DLL(const DLL &another) = delete;
+  DLL &operator=(const DLL &another) = delete;
 
-        if (node->prev == nullptr)
-            cout << "X\t";
-        else
-            cout << node->prev->data << "\t";
+  bool is_palindrome() {
 
-        cout << " <= [" << node->data << "]\t => \t";
-
-        if (node->next == nullptr)
-            cout << "X\t";
-        else
-            cout << node->next->data << "\t";
-
-        if (node == head)
-            cout << "head\n";
-        else if (node == tail)
-            cout << "tail\n";
-        else
-            cout << "\n";
-    }
-    void debug_print_list(string msg = "")
-    {
-        if (msg != "")
-            cout << msg << "\n";
-        for (int i = 0; i < (int)debug_data.size(); ++i)
-            debug_print_node(debug_data[i]);
-        cout << "************\n"
-             << flush;
-    }
-
-    string debug_to_string()
-    {
-        if (length == 0)
-            return "";
-        ostringstream oss;
-        for (Node *cur = head; cur; cur = cur->next)
-        {
-            oss << cur->data;
-            if (cur->next)
-                oss << " ";
-        }
-        return oss.str();
-    }
-
-    void debug_verify_data_integrity()
-    {
-        if (length == 0)
-        {
-            assert(head == nullptr);
-            assert(tail == nullptr);
-        }
-        else
-        {
-            assert(head != nullptr);
-            assert(tail != nullptr);
-            if (length == 1)
-                assert(head == tail);
-            else
-                assert(head != tail);
-            assert(!head->prev);
-            assert(!tail->next);
-        }
-        int len = 0;
-        for (Node *cur = head; cur; cur = cur->next, len++)
-        {
-            if (len == length - 1) // make sure we end at tail
-                assert(cur == tail);
-        }
-
-        assert(length == len);
-        assert(length == (int)debug_data.size());
-
-        len = 0;
-        for (Node *cur = tail; cur; cur = cur->prev, len++)
-        {
-            if (len == length - 1) // make sure we end at head
-                assert(cur == head);
-        }
-        cout << "\n";
-    }
-    ////////////////////////////////////////////////////////////
-
-    void print()
-    {
-        for (Node *cur = head; cur; cur = cur->next)
-            cout << cur->data << " ";
-        cout << "\n";
-    }
-
-    // These 2 simple functions just to not forget changing the vector and length
-    void delete_node(Node *node)
-    {
-        debug_remove_node(node);
-        --length;
-        delete node;
-    }
-
-    void add_node(Node *node)
-    {
-        debug_add_node(node);
-        ++length;
-    }
-
-    void link(Node *first, Node *second)
-    {
-        if (first)
-            first->next = second;
-        if (second)
-            second->prev = first;
-    }
-
-    void insert_end(int value)
-    {
-        Node *item = new Node(value);
-        add_node(item);
-
-        if (!head)
-            head = tail = item;
-        else
-        {
-            link(tail, item);
-            tail = item;
-        }
-        debug_verify_data_integrity();
-    }
-
-    void insert_front(int value)
-    {
-        Node *item = new Node(value);
-        add_node(item);
-
-        if (!head)
-            head = tail = item;
-        else
-        {
-            link(item, head);
-            head = item;
-        }
-        debug_verify_data_integrity();
-    }
-
-    void print_reversed()
-    {
-        for (Node *cur = tail; cur; cur = cur->prev)
-            cout << cur->data << " ";
-        cout << "\n";
-    }
-
-    void delete_front()
-    {
-        if (!head)
-            return;
-        Node *cur = head->next;
-        delete_node(head);
-        head = cur;
-
-        // Integrity change
-        if (head)
-            head->prev = nullptr;
-        else if (!length)
-            tail = nullptr;
-
-        debug_verify_data_integrity();
-    }
-
-    void delete_end()
-    {
-        if (!head)
-            return;
-        Node *cur = tail->prev;
-        delete_node(tail);
-        tail = cur;
-
-        // Integrity change
-        if (tail)
-            tail->next = nullptr;
-        else if (!length)
-            head = nullptr;
-
-        debug_verify_data_integrity();
-    }
-
-    Node *delete_and_link(Node *cur)
-    {
-        // remove this node, but connect its neighbors
-        Node *ret = cur->prev;
-        link(cur->prev, cur->next);
-        delete_node(cur);
-
-        return ret;
-    }
-
-    Node *delete_a_node(Node *cur)
-    {
-        if (head == cur)
-            head = cur->next;
-
-        if (cur == tail)
-            tail = cur->prev;
-        cur = delete_and_link(cur);
-
-        debug_verify_data_integrity();
-        return cur;
-    }
-
-    bool is_palindrome()
-    {
-        int i{0};
-        for (Node *front{head}, *back{tail}; i < (length / 2 + length % 2); front = front->next, back = back->prev)
-        {
-            if (front->data != back->data)
-                return false;
-            i++;
-        }
-
-        return true;
-    }
+    for (Node *lcur{head.get()}, *rcur{tail}; lcur->next.get() != rcur->prev;
+         lcur = lcur->next.get(), rcur = rcur->prev)
+      if (lcur->data != rcur->data)
+        return false;
+    return true;
+  }
 };
 
-void test1()
-{
-    cout << "\n\ntest 1\n";
-    LinkedList ll{1, 2, 2, 1};
+void test1() {
+  std::cout << "\n\ntest 1\n";
 
-    ll.print();
+  DLL ll{1, 2, 2, 1};
+  std::cout << ll << "\n";
 
-    bool result = ll.is_palindrome();
-    bool expected = true;
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult : " << result << "\n";
-        assert(false);
-    }
-    ll.debug_print_list("******************");
+  std::cout << "Is Palindrome : ";
+
+  bool result = ll.is_palindrome();
+  std::cout << result << "\n";
+
+  bool expected = true;
+  if (expected != result) {
+    std::cout << "no match:\nExpected: " << expected << "\nResult : " << result
+              << "\n";
+    assert(false);
+  }
+  ll.debug_print_list("******************");
 }
 
-void test2()
-{
-    cout << "\n\ntest 2\n";
-    LinkedList ll{1, 2, 3, 2, 1};
+void test2() {
+  std::cout << "\n\ntest 2\n";
 
-    ll.print();
+  DLL ll{1, 2, 3, 2, 1};
+  std::cout << ll << "\n";
 
-    bool result = ll.is_palindrome();
-    bool expected = true;
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult : " << result << "\n";
-        assert(false);
-    }
-    ll.debug_print_list("******************");
+  bool result = ll.is_palindrome();
+  std::cout << "Is Palindrome : " << result << "\n";
+
+  bool expected = true;
+  if (expected != result) {
+    std::cout << "no match:\nExpected: " << expected << "\nResult : " << result
+              << "\n";
+    assert(false);
+  }
+  ll.debug_print_list("******************");
 }
-void test3()
-{
-    cout << "\n\ntest 3\n";
-    LinkedList ll{1, 2, 3, 4, 2, 1};
+void test3() {
+  std::cout << "\n\ntest 3\n";
 
-    ll.print();
+  DLL ll{1, 2, 3, 4, 2, 1};
+  std::cout << ll << "\n";
 
-    bool result = ll.is_palindrome();
-    bool expected = false;
-    if (expected != result)
-    {
-        cout << "no match:\nExpected: " << expected << "\nResult : " << result << "\n";
-        assert(false);
-    }
-    ll.debug_print_list("******************");
+  bool result = ll.is_palindrome();
+  std::cout << "Is Palindrome : " << result << "\n";
+
+  bool expected = false;
+  if (expected != result) {
+    std::cout << "no match:\nExpected: " << expected << "\nResult : " << result
+              << "\n";
+    assert(false);
+  }
+  ll.debug_print_list("******************");
 }
-int main()
-{
-    test1();
-    test2();
-    test3();
-    cout << "\n\n NO RTE\n";
-    return 0;
+
+int main() {
+  test1();
+  test2();
+  test3();
+  std::cout << "\n\n NO RTE\n";
+  return 0;
 }
